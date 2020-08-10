0007ef706940ba4a23d7b25c8eab4326357fc9ff
Sheila Leveille <sbleveille@gmail.com>
2020-08-10 11:21:37 +1000

    ARCH: added active record files from the lecture

---
+++ app/models/restaurant.rb
@@ -0,0 +1,14 @@
+class Restaurant < ActiveRecord::Base
+    # attr_accessors :name, :address
+
+    # def initialize(attributes = {})
+    #     @id = attributes[:id]
+    #     ...
+    # end
+
+    # def self.all
+    #     DB.execute('SELECT * FROM restaurants')
+    # end
+
+    # ...
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
 # This is where you can create initial data for your app.
+require 'faker'
+
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+
+10.times do
+    restaurant = Restaurant.new(
+        name: Faker::Movies::HarryPotter.location,
+        address: Faker::Movies::HarryPotter.character
+    )
+    restaurant.save
+end
+
+puts "Created: #{Restaurant.count} restaurant(s)"
